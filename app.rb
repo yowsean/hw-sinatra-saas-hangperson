c53cc9e6cf7bcca6726739c766ccb94395778ce0
yowsean <yowseanli@gmail.com>
2019-02-10 15:36:48 -0800

    heroku deploy

--- app.rb
+++ app.rb
@@ -39,7 +39,7 @@ class HangpersonApp < Sinatra::Base
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
-    ### YOUR CODE HERE ###
+    @game.guess(letter)
     redirect '/show'
   end
   
@@ -49,7 +49,15 @@ class HangpersonApp < Sinatra::Base
   # Notice that the show.erb template expects to use the instance variables
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
-    ### YOUR CODE HERE ###
+    if @game.check_win_or_lose == :lose
+      redirect '/lose'
+    elsif @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :play
+      @game.word_with_guesses
+    else
+      redirect '/play'
+    end
     erb :show # You may change/remove this line
   end
   
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,4 +1,5 @@
 class HangpersonGame
+  attr_accessor :word, :guesses, :wrong_guesses
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
@@ -10,6 +11,53 @@ class HangpersonGame
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    if letter.nil?
+      raise ArgumentError, 'Guess is nil'
+    end
+    if letter.empty?
+      raise ArgumentError, 'Guess is empty'
+    end
+    if !/[a-zA-Z]/.match(letter)
+      raise ArgumentError, 'Guess is not a letter'
+    end
+    letter.downcase!
+    if !@guesses.include?(letter) and !@wrong_guesses.include?(letter)
+      if @word.downcase.include?(letter)
+        @guesses << letter
+      else
+        @wrong_guesses << letter
+      end
+      return true
+    else
+      return false
+    end
+  end
+
+  def word_with_guesses
+    output = @word
+    @word.chars do |letter|
+      if !@guesses.include?(letter)
+        output = output.tr(letter, '-')
+      end
+    end
+    return output
+  end
+
+  def check_win_or_lose
+    if @guesses.length + @wrong_guesses.length > 6
+      return :lose
+    end
+    @word.chars do |letter|
+      if !@guesses.include?(letter)
+        return :play
+      end
+    end
+    return :win
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
